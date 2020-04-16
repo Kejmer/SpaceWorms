b58efe0d8c51269d38503b474a0c5e14da8f8854
Kejmer <kajetanhus@gmail.com>
2020-04-16 21:19:19 +0200

    turn switch

--- include/spaceship.h
+++ include/spaceship.h
@@ -7,22 +7,26 @@
 
 class Spaceship : public Entity {
 public:
-    Spaceship(sf::Vector2f position);
+    Spaceship(sf::Vector2f position, sf::Color color = sf::Color::Red);
     void input(sf::Event event);
     void update(sf::Time dt);
     void draw(sf::RenderWindow& window);
 
     void shoot();
     void move(sf::Vector2f vector);
+    int getID();
 private:
     void realtimeInput();
     sf::Vector2f getDirection();
     void createHitbox();
-    
+
     static const float rotation_speed;
     static const float shots_per_second;
     static const float bullet_speed;
     static const float ship_speed;
+    static int counter;
+    int id;
+    int team_id;
 
     sf::CircleShape ship;
     int rotation;
--- src/world.cpp
+++ src/world.cpp
@@ -2,11 +2,17 @@
 #include "../include/collisions.h"
 
 const sf::Time World::frame_time = sf::seconds(1./60.);
+const sf::Time World::turn_time = sf::seconds(2.);
 
 World::World(sf::RenderWindow& window)
 : window(window)
 , entities()
-, is_time_flowing(true) {}
+, requesterId(0)
+, is_time_flowing(false)
+, current_team(0)
+, current_ship(0) {
+    time_left = turn_time;
+}
 
 void World::input() {
     sf::Event event;
@@ -22,10 +28,14 @@ void World::input() {
 void World::update(sf::Time dt) {
     for (auto& entity : entities)
         entity->update(dt);
-    // if poniżej to fragment prezentujący funkcjonalność,
-    // zostanie usunięty po dodaniu slidera czasu (życzenia inwestora)
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::T)) {
-        is_time_flowing = !is_time_flowing;
+
+    if (is_time_flowing)
+        time_left -= dt;
+
+    if (time_left <= sf::Time::Zero) {
+        nextTeam();
+        time_left = turn_time;
+        is_time_flowing = false;
     }
 
     checkCollisions();
@@ -46,6 +56,7 @@ void World::draw() {
 void World::run() {
     sf::Clock clock;
     sf::Time last_update = sf::Time::Zero;
+    nextTeam();
 
     while (window.isOpen()) {
         sf::Time elapsed = clock.restart();
@@ -104,6 +115,61 @@ bool Entity::isTimeFlowing() {
 void World::checkCollisions() {
     for (unsigned int i = 0; i < entities.size(); i++)
         for (unsigned int j = i + 1; j < entities.size(); j++)
-            if (entities[i]->doesCollide(*entities[j])) 
+            if (entities[i]->doesCollide(*entities[j]))
                 collide(entities[i].get(), entities[j].get());
+}
+
+void World::moveRequest(int spaceshipId) {
+    if (!is_time_flowing) {
+        is_time_flowing = true;
+        requesterId = spaceshipId;
+    }
+}
+
+int World::getMoveRequest() {
+    return requesterId;
+}
+
+void World::clearRequest() {
+    requesterId = 0;
+    is_time_flowing = false;
+}
+
+int World::newTeam(sf::Color color) {
+    auto ptr = std::make_shared<Team>(color);
+    ptr->setWorld(this);
+    teams.push_back(ptr);
+    return ptr->getID();
+}
+
+void World::newShip(sf::Vector2f postion, int teamID) {
+    for (auto t : teams) {
+        if (t->getID() == teamID) {
+            this->addEntity(t->addShip(postion));
+            break;
+        }
+    }
+}
+
+void World::controlNext() {
+    std::shared_ptr<Team> team = teams[current_team];
+    current_ship = team->nextShip();
+}
+
+int World::nextTeam() {
+    if (teams.size() == 0)
+        return 0;
+    current_team++;
+    current_team %= teams.size();
+    controlNext();
+    return current_team;
+}
+
+int World::getController() {
+    return current_ship;
+}
+
+void World::nextTurn() {
+    current_ship = 0;
+    is_time_flowing = true;
 }
