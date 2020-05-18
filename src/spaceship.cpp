bba184253e7a8bfce830739e16882de25f84b4bf
asledz <anita.gaja.sledz@gmail.com>
2020-05-19 01:03:15 +0200

    Merge + poprawki żeby działało z resztą

--- src/spaceship.cpp
+++ src/spaceship.cpp
@@ -34,16 +34,16 @@ Spaceship::Spaceship(sf::Vector2f position, std::string new_file)
     healthbar = std::shared_ptr<Bar>(new Bar{this, sf::Color::Green, sf::Color::Red, {50, 5}, {0, 40}, getStatistics(MaxHealth), getStatistics(Healthpoints)});
     addAttachable(healthbar);
 
-    shot_charge = std::shared_ptr<Bar>(new Bar{this, sf::Color{255, 157, 0}, sf::Color::White, {50, 5}, {0, -40}, getStatistics(ShotChargeTime), 0});
+    shot_charge = std::shared_ptr<Bar>(new Bar{this, sf::Color{255, 157, 0}, sf::Color::White, {50, 5}, {0, -30}, getStatistics(ShotChargeTime), 0});
 
     // Fuel bar. Ma ten sam kolor co shot_charge, ale inne tło. Kiedy charguje, jest przykryte przez charge.
-    fuel_bar = std::shared_ptr<Bar>(new Bar{this, sf::Color{255, 157, 0}, sf::Color::Transparent, {50, 5}, {0, -40}, getWorld()->turn_time.asSeconds(), 0});
+    fuel_bar = std::shared_ptr<Bar>(new Bar{this, sf::Color{255, 157, 0}, sf::Color::Transparent, {50, 5}, {0, -30}, getWorld()->turn_time.asSeconds(), 0});
     addAttachable(fuel_bar);
 
     ammo_text = std::shared_ptr<TextBox>(new TextBox{this, "Ammo: " + std::to_string((int)getStatistics(AmmoCount)), {0, 57}, 14});
     addAttachable(ammo_text);
 
-    curr_player_indicator = std::shared_ptr<AttachTriangle>(new AttachTriangle{this, sf::Color::Red, 5.f, {0, -30}, false});
+    curr_player_indicator = std::shared_ptr<AttachTriangle>(new AttachTriangle{this, sf::Color::Red, 5.f, {0, -50}, false});
     addAttachable(curr_player_indicator);
 
     createHitbox();
